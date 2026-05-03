4a6122ae9b35156af6c5477075ac64029f2ffd7d
Joe Grech Esposito <99688750+Joedotmt@users.noreply.github.com>
2026-05-03 21:06:58 +0200

    Done

---
+++ main.js
@@ -0,0 +1,44 @@
+function attachMenuToggle() {
+  const $nav = $('#main-nav');
+  const $icon = $('#menu-icon');
+
+  $('#menu-toggle').on('click', () => {
+    $nav.toggle();
+    $icon.attr('src', $nav.is(':visible') ? 'close.svg' : 'menu.svg');
+  });
+}
+
+async function loadHeaderAndFooter() {
+  const headerRes = await fetch('header.html');
+  const footerRes = await fetch('footer.html');
+
+  const headerHtml = await headerRes.text();
+  const footerHtml = await footerRes.text();
+
+  document.getElementById("site-header").innerHTML = headerHtml;
+  document.getElementById("site-footer").innerHTML = footerHtml;
+
+  attachMenuToggle()
+}
+
+loadHeaderAndFooter();
+
+function addToCart(product) {
+  alert(product + " was added to cart");
+}
+
+function toggleInfo(descId, btn) {
+  var desc = document.getElementById(descId);
+  desc.classList.toggle('visible');
+  btn.textContent = desc.classList.contains('visible') ? 'Hide Info' : 'Info';
+}
+
+$(document).ready(function() {
+  $('.carousel').slick({
+    dots: true,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    slidesToShow: 1
+  });
+});
